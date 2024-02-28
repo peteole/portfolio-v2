f1cb1d3eb000479000d8766ed3b26a84e762648c
Ole Petersen <peteole2707@gmail.com>
2024-02-28 20:45:36 +0100

    Capitalize and sort skills

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -3,7 +3,7 @@ import {Progress} from "@nextui-org/progress";
 import resume from "@/config/resume";
 
 export default function PricingPage() {
-	const skill_to_number={
+	const skill_to_number:Record<string,number>={
 		"Beginner": 25,
 		"Intermediate": 50,
 		"Advanced": 75,
@@ -11,19 +11,19 @@ export default function PricingPage() {
 	
 	}
 	const keywords=Array.from(new Set(resume.skills?.map((skill)=>skill.keywords).flat())).filter((keyword)=>keyword) as string[]
-	const skillsByKeywords=Object.fromEntries(keywords.map((keyword)=>[keyword, resume.skills?.filter((skill)=>skill.keywords?.includes(keyword||""))]))
-	keywords.sort((a,b)=>skillsByKeywords[b].length-skillsByKeywords[a].length)
+	const skillsByKeywords=Object.fromEntries(keywords.map((keyword)=>[keyword, resume.skills?.filter((skill)=>skill.keywords?.includes(keyword||"")).sort((a,b)=>(skill_to_number[b.level as string]||0)-(skill_to_number[a.level as string]||0))]))
+	keywords.sort((a,b)=>(skillsByKeywords[b]?.length||0)-(skillsByKeywords[a]?.length||0))
 	
 	return (
 		<div className="flex flex-wrap">
 			{keywords.map((keyword, index) => {
 				return (
 					<div key={index} className="m-6">
-						<h1 className="text-xl">{keyword}</h1>
+						<h1 className="text-xl">{keyword[0].toUpperCase()+keyword.slice(1)}</h1>
 						{skillsByKeywords[keyword]?.map((skill, index) => {
 							return (
 								<div key={index}>
-									<Progress label={skill.name} value={skill_to_number[skill.level]} />
+									<Progress label={skill.name} value={skill_to_number[skill.level as string]} />
 								</div>
 							)
 						})}
