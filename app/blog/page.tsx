caa4bb7a5d5c46551d59ed6bc2060665b2c686e8
Ole Petersen <peteole2707@gmail.com>
2024-02-28 22:17:08 +0100

    Implement blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,61 @@
+import CardList from "@/components/CardList";
 import { title } from "@/components/primitives";
+import resume from "@/config/resume";
+function findImageUrl(text:string, baseURL?:string) {
+    // Regular expression to match both absolute and relative image URLs
+    const imageUrlRegex = /(?:https?:\/\/[^\s]+\.(?:jpeg|jpg|gif|png|svg))|(?:\bimages\/\S+\.(?:jpeg|jpg|gif|png|svg)\b)/ig;
 
-export default function BlogPage() {
+    // Find all matching URLs in the text
+    const matches = text.matchAll(imageUrlRegex);
+
+    // Convert relative URLs to absolute URLs using the baseURL
+    const absoluteUrls = Array.from(matches, match => {
+        const url = match[0];
+        if (!url.startsWith('http')) {
+            return new URL(url, baseURL).href;
+        }
+        return url;
+    });
+	const regex = /<img[^>]+src\s*=\s*["']?([^"'\s]+)["']?[^>]*>/g;
+    const dataUrls: string[] = [];
+    let match;
+
+    while ((match = regex.exec(text)) !== null) {
+        const src = match[1];
+        if (src.startsWith("data:")) {
+            dataUrls.push(src);
+        }
+    }
+	absoluteUrls.push(...dataUrls)
+
+    // Return the first absolute URL
+    return absoluteUrls[0] || null;
+}
+
+
+export default async function BlogPage() {
+	const blogPosts=resume.publications?.filter((publication)=>publication.type==="blog")||[]
+	blogPosts.sort((a,b)=>(new Date(b.releaseDate||"").getTime())-(new Date(a.releaseDate||"").getTime()))
+	const blogPostContents=await Promise.all(blogPosts.map(async (blogPost)=>{
+		const response=await fetch(blogPost.url||"")
+		return response.text()
+	}))
+	//console.log(blogPostContents)
+	const blogPostImages=blogPostContents.map((content,i)=>findImageUrl(content,blogPosts[i].url)||"")
+	//console.log(blogPostImages)
 	return (
 		<div>
 			<h1 className={title()}>Blog</h1>
+			<CardList items={blogPosts.map((p,i)=>{
+				return {
+					title:p.name||"",
+					date:p.releaseDate,
+					entity:p.publisher,
+					description:p.summary?.slice(0,200)+"...",
+					image:blogPostImages[i],
+					url:p.url
+				}
+			})}/>
 		</div>
 	);
 }
--- components/CardListItem.tsx
+++ components/CardListItem.tsx
@@ -16,12 +16,20 @@ export type CardListItemProps = {
     entity?: string;
     demo?: string;
     image?: string;
+    date?: string;
 }
 const CardListItem: React.FC<CardListItemProps> = (props) => {
     const urlSrc = props.url && (imageUrls as any)[props.url];
     const demoSrc = props.demo && (imageUrls as any)[props.demo];
     const src = props.image || urlSrc || demoSrc;
     console.log(props.title, ": ", src);
+    const image=<Image
+    alt="Card background"
+    className="rounded-xl mx-auto"
+    src={src}
+    style={{ objectFit: "cover", height: 100 }}
+    height={100}
+    width={250} />
     return (
         <Card className="py-4 h-80 pt-0 w-80">
             <CardHeader>
@@ -37,17 +45,13 @@ const CardListItem: React.FC<CardListItemProps> = (props) => {
                     }
                     {props.url ? <Link href={props.url}><h4 className="font-bold text-large">{props.title}</h4></Link> : <h4 className="font-bold text-large">{props.title}</h4>}
                     {(props.startDate) && <p className="text-tiny">{props.startDate} - {(props.endDate || "now")}{props.entity && `, ${props.entity}`}</p>}
+                    {props.date && <p className="text-tiny">{props.date}{props.entity && `, ${props.entity}`}</p>}
                 </div>
             </CardHeader>
 
             <CardBody className="overflow-y-auto py-0">
-                {props.url && src && <Link target="blank" href={props.url}><Image
-                    alt="Card background"
-                    className="rounded-xl mx-auto"
-                    src={src}
-                    style={{ objectFit: "cover", height: 100 }}
-                    height={100}
-                    width={250} /></Link>}
+                {props.url && src && <Link target="blank" href={props.url}>{image}</Link>}
+                {!props.url && src && image}
                 {props.description?.split("\n").map((line, index) => <p key={index} className="text-sm">{line}</p>)}
                 {props.demo && <Button target="blank" as={Link} href={props.demo} color="primary" size="sm" className="w-fit m-auto">View Demo</Button>}
             </CardBody>
